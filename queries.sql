81aeeae00d97bcc13a1e144c0b5922447c627dc5
MosDevx <mosesk.wamae@gmail.com>
2023-01-28 10:49:05 +0300

    Applied changes

--- queries.sql
+++ queries.sql
@@ -17,18 +17,21 @@ BEGIN;
 UPDATE animals SET species = 'unspecified';
 SELECT * FROM animals;
 ROLLBACK;
+SELECT * FROM animals;
 
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 SELECT * FROM animals;
 COMMIT;
+SELECT * FROM animals;
 
 
 BEGIN;
 DELETE FROM animals;
 SELECT * FROM animals;
 ROLLBACK;
+SELECT * FROM animals;
 
 
 BEGIN;
