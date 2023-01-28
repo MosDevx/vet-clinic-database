5c8df0d0813a8fc2e5903a09ec5c746d865d2436
MosDevx <mosesk.wamae@gmail.com>
2023-01-28 11:25:42 +0300

    edit schema

--- schema.sql
+++ schema.sql
@@ -2,6 +2,5 @@
 
 CREATE TABLE animals ( id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name TEXT NOT NULL, date_of_birth DATE NOT NULL, escape_attempts INT NOT NULL, neutered BOOLEAN NOT NULL,weight_kg DECIMAL NOT NULL);
 
-ALTER TABLE animals
-ADD COLUMN species text;
+ ALTER TABLE animals ADD  species varchar(255);
 
